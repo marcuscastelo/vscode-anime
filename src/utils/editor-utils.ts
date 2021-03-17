bf092c78cd626fda0585b3735452f64f77b76677
marcuscastelo <viniciuscastelo6@hotmail.com>
2021-03-17 20:18:50 -0300

    Fix keybinds bug

--- src/utils/editor-utils.ts
+++ src/utils/editor-utils.ts
@@ -1,5 +1,5 @@
 import { TextEditor } from "vscode";
 
 export function isEditingSimpleCursor(textEditor: TextEditor) {
-	return textEditor.selections.length === 1 && textEditor.selection.isSingleLine && textEditor.selection.start === textEditor.selection.end;
+	return textEditor.selections.length === 1 && textEditor.selection.isSingleLine;
 }
