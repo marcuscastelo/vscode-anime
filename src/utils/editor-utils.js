ded0887e84a59c8e3d01d818b54f4c320d4c546d
marcuscastelo <viniciuscastelo6@gmail.com>
2022-03-09 22:44:34 -0300

    Remove wrong out files inside src folder

--- src/utils/editor-utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEditingSimpleCursor = void 0;
-function isEditingSimpleCursor(textEditor) {
-    return textEditor.selections.length === 1 && textEditor.selection.isSingleLine;
-}
-exports.isEditingSimpleCursor = isEditingSimpleCursor;
-//# sourceMappingURL=editor-utils.js.map
