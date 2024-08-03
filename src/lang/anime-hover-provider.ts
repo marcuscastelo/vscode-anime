e5cdc1cdf4bb84131e21cc7842dd1c3fa02e599c
marcuscastelo <marcuscastelo@usp.br>
2024-08-03 13:11:35 -0300

    refactor: conform hover to prettier

--- src/lang/anime-hover-provider.ts
+++ src/lang/anime-hover-provider.ts
@@ -1,101 +1,132 @@
 import { isOk } from "rustic";
-import { CancellationToken, ExtensionContext, Hover, HoverProvider, languages, MarkdownString, Position, TextDocument, window } from "vscode";
+import {
+  CancellationToken,
+  ExtensionContext,
+  Hover,
+  HoverProvider,
+  languages,
+  MarkdownString,
+  Position,
+  TextDocument,
+  window,
+} from "vscode";
 import { LANGUAGE_ID } from "../constants";
 
 import { MarucsAnime } from "../extension";
-import LineContext from "../list-parser/line-context";
 import LineContextFinder from "../list-parser/line-context-finder";
 import { MAL } from "../services/mal";
 
 async function searchMAL(animeTitle: string) {
-    let foundAnimes = await MAL.searchAnime(animeTitle);
+  const foundAnimes = await MAL.searchAnime(animeTitle);
 
-    if (foundAnimes.length === 0) {
-        return undefined;
-    }
+  if (foundAnimes.length === 0) {
+    return undefined;
+  }
 
-    if (foundAnimes[0].titles.filter(title => title.title.toLowerCase() === animeTitle.toLowerCase()).length === 0) {
-        return undefined;
-    }
+  if (
+    foundAnimes[0].titles.filter(
+      (title) => title.title.toLowerCase() === animeTitle.toLowerCase(),
+    ).length === 0
+  ) {
+    return undefined;
+  }
 
-    return foundAnimes[0];
+  return foundAnimes[0];
 }
 
 export default class ShowHoverProvider implements HoverProvider {
-    public static register(context: ExtensionContext) {
-        const provider = new ShowHoverProvider(context);
-        return languages.registerHoverProvider(this.viewType, provider);
+  public static register(context: ExtensionContext) {
+    const provider = new ShowHoverProvider(context);
+    return languages.registerHoverProvider(this.viewType, provider);
+  }
+
+  private static readonly viewType = LANGUAGE_ID;
+
+  constructor(private readonly context: ExtensionContext) {}
+
+  private generateLineContextMD(document: TextDocument, line: number) {
+    const searchResult = LineContextFinder.findContext(document, line);
+
+    if (!isOk(searchResult)) {
+      const md = new MarkdownString();
+      md.appendMarkdown(`### ERROR: `);
+      md.appendText(`${searchResult.data.message}`);
+      return md;
     }
 
-    private static readonly viewType = LANGUAGE_ID;
+    const lineContext = searchResult.data;
+    const { currentDateLine, currentShowLine, currentTagsLines } = lineContext;
 
-    constructor(private readonly context: ExtensionContext) { }
-
-    private generateLineContextMD(document: TextDocument, line: number) {
-        const searchResult = LineContextFinder.findContext(document, line);
-        
-        if (!isOk(searchResult)) {
-            const md = new MarkdownString();
-            md.appendMarkdown(`### ERROR: `);
-            md.appendText(`${searchResult.data.message}`);
-            return md;
-        }
-
-        const lineContext = searchResult.data;
-        const { currentDateLine, currentShowLine, currentTagsLines } = lineContext;
-
-        if (!currentDateLine || !currentShowLine) {
-            const md = new MarkdownString();
-            md.appendMarkdown(`### ERROR: `);
-            md.appendText(`${"currentDateLine or currentShowLine are undefined"}`);
-            return md;
-        }
-        
-        const tagNames = currentTagsLines.map(lineInfo => lineInfo.params.tag.name);
-        const tagNamesString = tagNames.join(', ');
-
-        const show = MarucsAnime.INSTANCE.showStorage.searchShow(currentShowLine.params.showTitle);
-        let showLastTitle = 'NOT FOUND';
-        let showLastDate = 'NOT FOUND';
-        let showLastTagNamesString = 'NOT FOUND';
-        if (show) {
-            const firstMentionedContext = LineContextFinder.findContext(document, show.info.firstMentionedLine+1);
-            if (isOk(firstMentionedContext)) {
-                showLastTitle = firstMentionedContext.data.currentShowLine?.params.showTitle || 'EMPTY';
-                showLastDate = firstMentionedContext.data.currentDateLine?.params.date || "EMPTY";
-                showLastTagNamesString = firstMentionedContext.data.currentTagsLines.map(lineInfo => lineInfo.params.tag).join(', ');
-            }
-        }
-
-        return new MarkdownString(
-            `\n\nCurrent Line Context:` +
-            `\n\n  - Current Date: ${currentDateLine.params.date}` +
-            `\n\n  - Current Show: ${currentShowLine.params.showTitle}` +
-            `\n\n  - Current Tags: [${tagNamesString}]` +
-            `\n\nFirst Mention Show Context:` +
-            `\n\n  - First Mention Date: ${showLastDate}` +
-            `\n\n  - First Mention Show: ${showLastTitle}` +
-            `\n\n  - First Mention Tags: [${showLastTagNamesString}]`
-        );
+    if (!currentDateLine || !currentShowLine) {
+      const md = new MarkdownString();
+      md.appendMarkdown(`### ERROR: `);
+      md.appendText(`${"currentDateLine or currentShowLine are undefined"}`);
+      return md;
     }
 
-    public async provideHover(document: TextDocument, position: Position, token: CancellationToken) {
-        const extension = MarucsAnime.INSTANCE;
-        
-        if (!window.activeTextEditor) { return; }
+    const tagNames = currentTagsLines.map(
+      (lineInfo) => lineInfo.params.tag.name,
+    );
+    const tagNamesString = tagNames.join(", ");
 
-        const lineContext = LineContextFinder.findContext(document, position.line);
-        if (!isOk(lineContext)) {
-            return;
-        }
-
-        const malData = await searchMAL(lineContext.data.currentShowLine.params.showTitle);
-        const lineContextMD = new MarkdownString(
-            `![MAL](${malData?.images.jpg.image_url})`
-        );
-
-        const newMD = this.generateLineContextMD(document, position.line);
-        
-        return new Hover([lineContextMD, newMD]);
+    const show = MarucsAnime.INSTANCE.showStorage.searchShow(
+      currentShowLine.params.showTitle,
+    );
+    let showLastTitle = "NOT FOUND";
+    let showLastDate = "NOT FOUND";
+    let showLastTagNamesString = "NOT FOUND";
+    if (show) {
+      const firstMentionedContext = LineContextFinder.findContext(
+        document,
+        show.info.firstMentionedLine + 1,
+      );
+      if (isOk(firstMentionedContext)) {
+        showLastTitle =
+          firstMentionedContext.data.currentShowLine?.params.showTitle ||
+          "EMPTY";
+        showLastDate =
+          firstMentionedContext.data.currentDateLine?.params.date || "EMPTY";
+        showLastTagNamesString = firstMentionedContext.data.currentTagsLines
+          .map((lineInfo) => lineInfo.params.tag)
+          .join(", ");
+      }
     }
-};
+
+    return new MarkdownString(
+      `\n\nCurrent Line Context:` +
+        `\n\n  - Current Date: ${currentDateLine.params.date}` +
+        `\n\n  - Current Show: ${currentShowLine.params.showTitle}` +
+        `\n\n  - Current Tags: [${tagNamesString}]` +
+        `\n\nFirst Mention Show Context:` +
+        `\n\n  - First Mention Date: ${showLastDate}` +
+        `\n\n  - First Mention Show: ${showLastTitle}` +
+        `\n\n  - First Mention Tags: [${showLastTagNamesString}]`,
+    );
+  }
+
+  public async provideHover(
+    document: TextDocument,
+    position: Position,
+    _token: CancellationToken,
+  ) {
+    if (!window.activeTextEditor) {
+      return;
+    }
+
+    const lineContext = LineContextFinder.findContext(document, position.line);
+    if (!isOk(lineContext)) {
+      return;
+    }
+
+    const malData = await searchMAL(
+      lineContext.data.currentShowLine.params.showTitle,
+    );
+    const lineContextMD = new MarkdownString(
+      `![MAL](${malData?.images.jpg.image_url})`,
+    );
+
+    const newMD = this.generateLineContextMD(document, position.line);
+
+    return new Hover([lineContextMD, newMD]);
+  }
+}
