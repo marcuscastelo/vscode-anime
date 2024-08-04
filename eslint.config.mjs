e521036c23cc358594e0f8f1fb056634a2368a62
marcuscastelo <marcuscastelo@usp.br>
2024-08-03 21:49:17 -0300

    dev: change unused to warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": [
-        "error",
+        "warn",
         {
           args: "all",
           argsIgnorePattern: "^_",
